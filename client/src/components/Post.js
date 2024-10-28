144d84c8c2760cbe96a33adc66942f0add63a540
LerichO <Lerich.Osay@UTDallas.edu>
2024-10-28 13:28:56 -0500

    clean up css

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const Post = (props) => {
+
+  const params = useParams();
+
   return (
     <div>
       <h2>{props.title}</h2>
+      <h2>{params.id}</h2>
       <p>{props.description}</p>
     </div>
   )
